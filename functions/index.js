a11ac688993b3ebad541d136449903842aa85be9
Maxwell Shick <maxwellshick@gmail.com>
2022-09-12 18:22:37 -0400

    correcting cors issue

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ const PORT = 80;
 
 // Parsing each request that comes into server
 connectDB();
-app.use(cors({ origin: true }));
+app.use(cors({ origin: 'https://nots-admin-dashboard.web.app' }));
 app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // url encoded form data
