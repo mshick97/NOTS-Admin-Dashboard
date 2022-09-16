35bb9e6a9c4a1deb93b68a1bd17ab58f7b144419
Maxwell Shick <maxwellshick@gmail.com>
2022-09-16 17:31:49 -0400

    testing firebase rewrite

--- client/api/axios.js
+++ client/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = 'https://us-central1-nots-admin-dashboard.cloudfunctions.net/server';
 
 export const axiosPublic = axios.create({
-  // baseURL: BASE_URL
+  // baseURL: BASE_URL;
 });
 
 export const axiosPrivate = axios.create({
