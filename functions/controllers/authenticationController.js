4e92386e02a1785463ce6fb99f589fef498b911a
Maxwell Shick <maxwellshick@gmail.com>
2022-11-23 17:02:47 -0500

    fixed rerender issue, previous cookie not being cleared

--- functions/controllers/authenticationController.js
+++ functions/controllers/authenticationController.js
@@ -8,17 +8,13 @@ authenticationController.verifyAccessJWT = (req, res, next) => {
   const token = req.headers['authorization']; // storing token in header object with key of authorization
   if (!token) return res.sendStatus(401); // returns if there is no token passed in
 
-  jwt.verify(
-    token,
-    process.env.ACCESS_TOKEN_SECRET,
-    (err, decoded) => {
-      if (err) return res.sendStatus(403); // if the token doesn't match up with the secret, could have been tampered with
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403); // if the token doesn't match up with the secret, could have been tampered with
 
-      req.user = decoded.username;
-      return next();
-    }
-  );
-}
+    req.user = decoded.username;
+    return next();
+  });
+};
 
 authenticationController.handleRefreshToken = (req, res, next) => {
   const cookies = req.cookies;
@@ -30,33 +26,28 @@ authenticationController.handleRefreshToken = (req, res, next) => {
       return next({
         log: 'Error has occurred in the handleRefreshToken middleware in authenticationController',
         status: 500,
-        message: { err: err }
+        message: { err: err },
       });
     }
 
     if (!tokenRes) {
       res.clearCookie('__session', { httpOnly: true, sameSite: 'None', secure: true });
-      return res.sendStatus(403);
+      return res.status(403).json(false);
     }
 
     if (tokenRes) {
-      jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET,
-        (error, decoded) => {
-          if (error || tokenRes.email !== decoded.username) return res.sendStatus(403);
-
-          const accessToken = jwt.sign(
-            { username: decoded.username },
-            process.env.ACCESS_TOKEN_SECRET,
-            { expiresIn: '10s' }
-          );
-
-          return res.json({ accessToken: accessToken });
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (error, decoded) => {
+        if (error || tokenRes.email !== decoded.username) {
+          res.clearCookie('__session', { httpOnly: true, sameSite: 'None', secure: true });
+          return res.status(403).json(false);
         }
-      )
+
+        const accessToken = jwt.sign({ username: decoded.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10s' });
+
+        return res.json({ accessToken: accessToken });
+      });
     }
   });
-}
+};
 
 module.exports = authenticationController;
