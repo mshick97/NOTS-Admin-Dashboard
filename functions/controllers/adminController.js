96a751a084f43086fcb932b9fd67bea7b07f8b31
Maxwell Shick <maxwellshick@gmail.com>
2022-12-02 13:01:05 -0500

    changed the admin authentication to be more modular to avoid nesting try catch blocks

--- functions/controllers/adminController.js
+++ functions/controllers/adminController.js
@@ -32,37 +32,51 @@ adminController.adminLogin = async (req, res, next) => {
         return res.status(401).json(res.locals); // frontend expecting an object with the validLogin property
       }
 
-      // Creating access token for short session access to APIs and storing the refresh token to refresh access tokens
-      const accessToken = jwt.sign(
-        { username: adminResults.email },
-        process.env.ACCESS_TOKEN_SECRET,
-        // { expiresIn: '600s' }
-        { expiresIn: '10s' }
-      );
-
-      const refreshToken = jwt.sign(
-        { username: adminResults.email },
-        process.env.REFRESH_TOKEN_SECRET,
-        // { expiresIn: '1d' }
-        { expiresIn: '40s' }
-      );
-
-      const addTokenQuery = 'UPDATE "users"."admins" SET "refresh_token" = $1 WHERE "email" = $2 RETURNING *;';
-      await usersDB.query(addTokenQuery, [refreshToken, email]);
-
-      res.locals.isAdmin = {
-        validLogin: true,
-        accessToken: accessToken,
-        refreshToken: refreshToken,
-        adminName: { firstName: adminResults.first_name, lastName: adminResults.last_name },
-      };
-
-      res.setHeader('Cache-Control', 'private').cookie('__session', refreshToken, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 }); // setting max age to one day + sending back the refresh token as the cookie + not accessible by JS
+      res.locals.adminResults = adminResults;
       return next();
     });
   } catch (err) {
     return next({
       log: 'Error occurred in the adminController: ' + err,
+      status: 400,
+      message: 'An error occurred when logging in',
+    });
+  }
+};
+
+adminController.addRefreshToken = async (req, res, next) => {
+  const { email, first_name, last_name } = res.locals.adminResults;
+  // Creating access token for short session access to APIs and storing the refresh token to refresh access tokens
+  const accessToken = jwt.sign(
+    { username: email },
+    process.env.ACCESS_TOKEN_SECRET,
+    // { expiresIn: '600s' }
+    { expiresIn: '10s' }
+  );
+
+  const refreshToken = jwt.sign(
+    { username: email },
+    process.env.REFRESH_TOKEN_SECRET,
+    // { expiresIn: '1d' }
+    { expiresIn: '40s' }
+  );
+
+  const addTokenQuery = 'UPDATE "users"."admins" SET "refresh_token" = $1 WHERE "email" = $2 RETURNING *;';
+  try {
+    await usersDB.query(addTokenQuery, [refreshToken, email]);
+
+    res.locals.isAdmin = {
+      validLogin: true,
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      adminName: { firstName: first_name, lastName: last_name },
+    };
+
+    res.setHeader('Cache-Control', 'private').cookie('__session', refreshToken, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 }); // setting max age to one day + sending back the refresh token as the cookie + not accessible by JS
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error occurred in the addRefreshToken in adminController: ' + err,
       status: 400,
       message: 'An error occurred when logging in',
     });
--- functions/routes/authRoutes.js
+++ functions/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const Router = express.Router();
 
-const { adminLogin } = require('../controllers/adminController');
+const { adminLogin, addRefreshToken } = require('../controllers/adminController');
 
-Router.post('/', adminLogin, (req, res) => {
+Router.post('/', adminLogin, addRefreshToken, (req, res) => {
   console.log('returning: adminController.adminLogin');
   return res.status(200).json(res.locals.isAdmin);
 });
