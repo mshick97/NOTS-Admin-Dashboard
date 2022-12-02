6af1238bf9748c01ad050e387563c09129ffaceb
Maxwell Shick <maxwellshick@gmail.com>
2022-12-02 13:16:20 -0500

    reordered tests to see if this fixes deploy issue

--- cypress/e2e/login/loginFunctionality.cy.js
+++ cypress/e2e/login/loginFunctionality.cy.js
@@ -1,4 +1,13 @@
 describe('login functionality', () => {
+  it('should render the login view at the root URL', () => {
+    cy.visit('/', { timeout: 10000 });
+    cy.url().should('include', '/login');
+  });
+
+  it('should render the login component', () => {
+    cy.get('#loginContainer');
+  });
+
   const loginCreds = {
     email: 'maxwellshick@gmail.com',
     password: 'maxwell123',
