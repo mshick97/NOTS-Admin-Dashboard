a62cef3181cec0dbdd426ee2b3c974dcdfd729f1
Maxwell Shick <maxwellshick@gmail.com>
2022-12-01 22:32:15 -0500

    first set of tests now passing, second set fail for some reason

--- cypress/e2e/login/loginView.cy.js
+++ cypress/e2e/login/loginView.cy.js
@@ -1,6 +1,6 @@
 describe('rendering app first time', () => {
   it('should render the login view at the root URL', () => {
-    cy.visit('/');
+    cy.visit('/', { timeout: 10000 });
     cy.url().should('include', '/login');
   });
 
