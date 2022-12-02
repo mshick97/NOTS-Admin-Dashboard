54e4e890e9887220c800c03005932c37b1ef1aaf
Maxwell Shick <maxwellshick@gmail.com>
2022-12-02 16:06:27 -0500

    trying to add more try catch blocks

--- client/hooks/useAxiosPrivate.jsx
+++ client/hooks/useAxiosPrivate.jsx
@@ -8,36 +8,40 @@ const useAxiosPrivate = () => {
   const { auth } = useAuth();
 
   useEffect(() => {
-    const requestIntercept = axiosPrivate.interceptors.request.use(
-      (config) => {
-        if (!config.headers['authorization']) {
-          config.headers['authorization'] = auth.accessToken;
+    try {
+      const requestIntercept = axiosPrivate.interceptors.request.use(
+        (config) => {
+          if (!config.headers['authorization']) {
+            config.headers['authorization'] = auth.accessToken;
+          }
+          return config;
+        },
+        (error) => {
+          return Promise.reject(error);
         }
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error);
-      }
-    );
+      );
 
-    const responseIntercept = axiosPrivate.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        const prevRequest = error?.config;
-        if (error?.response?.status === 403 && !prevRequest?.sent) {
-          prevRequest.sent = true;
-          const newAccessToken = await refresh();
-          prevRequest.headers['authorization'] = newAccessToken;
-          return axiosPrivate(prevRequest);
+      const responseIntercept = axiosPrivate.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+          const prevRequest = error?.config;
+          if (error?.response?.status === 403 && !prevRequest?.sent) {
+            prevRequest.sent = true;
+            const newAccessToken = await refresh();
+            prevRequest.headers['authorization'] = newAccessToken;
+            return axiosPrivate(prevRequest);
+          }
+          return Promise.reject(error);
         }
-        return Promise.reject(error);
-      }
-    );
+      );
 
-    return () => {
-      axiosPrivate.interceptors.response.eject(requestIntercept);
-      axiosPrivate.interceptors.response.eject(responseIntercept);
-    };
+      return () => {
+        axiosPrivate.interceptors.response.eject(requestIntercept);
+        axiosPrivate.interceptors.response.eject(responseIntercept);
+      };
+    } catch (err) {
+      console.log(err);
+    }
   }, [auth, refresh]);
 
   return axiosPrivate;
