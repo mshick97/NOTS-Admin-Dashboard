733a0a97dcb0991493bf85f8e50f056cf4661f00
Maxwell Shick <maxwellshick@gmail.com>
2022-12-02 15:32:29 -0500

    tests passing in GitHub Actions, but uncaught error causing issue

--- cypress/e2e/login/loginFunctionality.cy.js
+++ cypress/e2e/login/loginFunctionality.cy.js
@@ -1,13 +1,11 @@
 describe('login functionality', () => {
   it('should render the login view at the root URL', () => {
-    cy.on('uncaught:exception', (err, runnable, promise) => {
+    cy.visit('/', { timeout: 10000 }).on('uncaught:exception', (err, runnable, promise) => {
       if (promise) {
         console.log(err);
         return false;
       }
     });
-
-    cy.visit('/', { timeout: 10000 });
     cy.url().should('include', '/login');
   });
 
@@ -21,45 +19,41 @@ describe('login functionality', () => {
   };
 
   it('should allow users to input a username and password', () => {
-    cy.on('uncaught:exception', (err, runnable, promise) => {
+    cy.visit('/').on('uncaught:exception', (err, runnable, promise) => {
       if (promise) {
         console.log(err);
         return false;
       }
     });
-
-    cy.visit('/');
 
     cy.get('input[name=email]').type(loginCreds.email);
     cy.get('input[name=password]').type(loginCreds.password);
   });
 
   it('should submit the email and password + navigate to orders', () => {
-    cy.on('uncaught:exception', (err, runnable, promise) => {
-      if (promise) {
-        console.log(err);
-        return false;
-      }
-    });
-
-    cy.get('#loginButton').click();
+    cy.get('#loginButton')
+      .click()
+      .on('uncaught:exception', (err, runnable, promise) => {
+        if (promise) {
+          console.log(err);
+          return false;
+        }
+      });
 
     cy.url({ timeout: 15000 }).should('include', '/orders');
     cy.get('#ordersContainer');
   });
 
   it('user should receive a session cookie on login', () => {
-    cy.on('uncaught:exception', (err, runnable, promise) => {
-      if (promise) {
-        console.log(err);
-        return false;
-      }
-    });
-
     cy.request({
       method: 'POST',
       url: 'http://localhost:3000/api/auth',
       body: loginCreds,
+    }).on('uncaught:exception', (err, runnable, promise) => {
+      if (promise) {
+        console.log(err);
+        return false;
+      }
     });
     cy.getCookie('__session', { timeout: 15000 }).should('exist');
   });
