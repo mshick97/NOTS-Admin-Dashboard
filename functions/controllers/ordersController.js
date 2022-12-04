3f373c61bc4c2cf9689e04caca1ded6a7d0b4148
Maxwell Shick <maxwellshick@gmail.com>
2022-12-04 15:10:43 -0500

    added try catch block to webflow controller

--- functions/controllers/ordersController.js
+++ functions/controllers/ordersController.js
@@ -10,51 +10,57 @@ const ordersController = {};
 ordersController.getOrders = async (req, res, next) => {
   const WEBFLOW_ORDERS_URL = `${WEBFLOW_API_URL}/sites/${NOTS_HAIR_SITE_ID}/orders`;
 
-  const orders = await axios.get(WEBFLOW_ORDERS_URL, {
-    headers: { Authorization: `Bearer ${WEBFLOW_API_KEY}` },
-  });
+  try {
+    const orders = await axios.get(WEBFLOW_ORDERS_URL, {
+      headers: { Authorization: `Bearer ${WEBFLOW_API_KEY}` },
+    });
 
-  const ordersData = [];
+    const ordersData = [];
 
-  orders.data.forEach((orderDetails) => {
-    const {
-      acceptedOn,
-      billingAddress,
-      comment,
-      customerInfo,
-      customerPaid,
-      netAmount,
-      orderComment,
-      orderId,
-      paypalDetails,
-      purchasedItems,
-      status,
-      stripeDetails,
-      totals,
-    } = orderDetails;
+    orders.data.forEach((orderDetails) => {
+      const {
+        acceptedOn,
+        billingAddress,
+        comment,
+        customerInfo,
+        customerPaid,
+        netAmount,
+        orderComment,
+        orderId,
+        paypalDetails,
+        purchasedItems,
+        status,
+        stripeDetails,
+        totals,
+      } = orderDetails;
 
-    const order = {
-      acceptedOn: acceptedOn,
-      billingAddress: billingAddress,
-      comment: comment,
-      customerInfo: customerInfo,
-      customerPaid: customerPaid,
-      netAmount: netAmount,
-      orderComment: orderComment,
-      orderId: orderId,
-      paypalDetails: paypalDetails,
-      purchasedItems: purchasedItems,
-      status: status,
-      stripeDetails: stripeDetails,
-      totals: totals,
-    };
+      const order = {
+        acceptedOn: acceptedOn,
+        billingAddress: billingAddress,
+        comment: comment,
+        customerInfo: customerInfo,
+        customerPaid: customerPaid,
+        netAmount: netAmount,
+        orderComment: orderComment,
+        orderId: orderId,
+        paypalDetails: paypalDetails,
+        purchasedItems: purchasedItems,
+        status: status,
+        stripeDetails: stripeDetails,
+        totals: totals,
+      };
 
-    ordersData.push(order);
-  });
+      ordersData.push(order);
+    });
 
-  res.locals.orderData = ordersData;
-
-  return next();
+    res.locals.orderData = ordersData;
+    return next();
+  } catch (err) {
+    return next({
+      log: 'Error fetching order data from Webflow in ordersController: ' + err,
+      status: 400,
+    });
+  }
 };
 
 module.exports = ordersController;
