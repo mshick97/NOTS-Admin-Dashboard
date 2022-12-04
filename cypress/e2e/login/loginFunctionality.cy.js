aef107deab52a487a24430ab319813eeb8059319
Maxwell Shick <maxwellshick@gmail.com>
2022-12-04 14:01:49 -0500

    removed second url visit

--- cypress/e2e/login/loginFunctionality.cy.js
+++ cypress/e2e/login/loginFunctionality.cy.js
@@ -24,7 +24,6 @@ describe('login functionality', () => {
       cy.log(err);
       return false;
     });
-    cy.visit('/login');
 
     cy.get('input[name=email]').type(loginCreds.email);
     cy.get('input[name=password]').type(loginCreds.password);
